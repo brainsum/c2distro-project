a8c63008cb2d57789fe48ff46a29151f95436d24
szatom <szanyitomi@gmail.com>
2021-09-29 09:17:39 +0200

    C2D-3 | Changed grumphp, added git pre* setitngs to startup.

--- startup.sh
+++ startup.sh
@@ -4,10 +4,18 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
 VARIABLES=$(realpath "${SCRIPT_DIR}/docker/acr.env")
-# shellcheck source=docker/acr.env
+#shellcheck source=docker/acr.env
 source "${VARIABLES}"
 
 az acr login -n "${REGISTRY_NAME}" --subscription "${SUBSCRIPTION_NAME}"
+
+if [ ! -f .git/hooks/pre-commit ]; then
+    cp example.git-pre-commit .git/hooks/pre-commit
+fi
+
+if [ ! -f .git/hooks/commit-msg ]; then
+    cp example.git-commit-msg .git/hooks/commit-msg
+fi
 
 COMPOSE_FILES="-f docker-compose.yml"
 
@@ -19,4 +27,4 @@ echo "Using compose files: ${COMPOSE_FILES}"
 
 docker-compose ${COMPOSE_FILES} up -d --force-recreate || exit 1
 docker-compose ${COMPOSE_FILES} ps || exit 1
-docker-compose ${COMPOSE_FILES} exec php sh || exit 1
+docker-compose ${COMPOSE_FILES} exec php bash || exit 1
